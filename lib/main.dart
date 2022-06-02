a30ba7d2a8ca904e26e91feb44c6d009631a47c3
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2022-06-02 20:33:24 +0200

    Add buttons

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,33 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What is your name?",
+      "What is your quest?",
+      "What is your favorite color?"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text("My first page"),
         ),
-        body: const Text("This is my default text"),
+        body: Column(
+          children: const [
+            Text("The question"),
+            RaisedButton(
+              onPressed: null,
+              child: Text("Answer 1"),
+            ),
+            RaisedButton(
+              onPressed: null,
+              child: Text("Answer 2"),
+            ),
+            RaisedButton(
+              onPressed: null,
+              child: Text("Answer 3"),
+            ),
+          ],
+        ),
       ),
     );
   }
